a454b4e28095dcdbd7542c45ccdb7bf88f87c9da
AMISETTI N V VAMSI KRISHNA <41267563+YoungCoderVamsi@users.noreply.github.com>
2020-03-17 13:58:39 +0530

    Solved Division.py
    Constant Time Complexity

--- Problems/Division.py
+++ Problems/Division.py
@@ -1,1 +1,16 @@
+def Solve(a,b,c):
+  if(b>a):
+    return -1
+  elif(c<a):
+    if(c%a==b):
+      return c
+    else:
+      return -1
+  else:
+    return c-(c%a)-(a-b)
 
+T=int(input())
+for i in range(T):
+  A=list(map(int,input().split()))
+  out_=Solve(A[0],A[1],A[2])
+  print(out_)
